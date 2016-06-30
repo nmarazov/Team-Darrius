2f59ee0087e174a3865daead0496356fdfaab4f8
nmarazov <nmarazov@abv.bg>
2016-06-30 16:33:01 +0300

    added inheritance. + some properties in IFamilyMember

--- FamilyBudgetCalculator/BudgetCalculator.Core/Family/FamilyMember.cs
+++ FamilyBudgetCalculator/BudgetCalculator.Core/Family/FamilyMember.cs
@@ -3,9 +3,33 @@
 
     using System;
     using System.Collections.Generic;
+    using Enum;
     using Interface;
 
     public class FamilyMember : IFamilyMember
     {
+        public sbyte Age
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Status Status
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
--- FamilyBudgetCalculator/BudgetCalculator.Core/Interface/IFamilyMember.cs
+++ FamilyBudgetCalculator/BudgetCalculator.Core/Interface/IFamilyMember.cs
@@ -1,11 +1,13 @@
 ﻿namespace BudgetCalculator.Core.Interface
 {
+    using Enum;
+
     public interface IFamilyMember
     {
         string Name { get; }
 
         sbyte Age { get; }
 
-
+        Status Status { get; }
     }
 }
