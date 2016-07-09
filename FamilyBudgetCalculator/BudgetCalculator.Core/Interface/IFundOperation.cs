15312871f0d44c9520307a74105b387aa5f0884c
Stefan <st_dantchev@abv.bg>
2016-07-09 22:06:18 +0300

    Предложение за схема на основните класове

---
+++ FamilyBudgetCalculator/BudgetCalculator.Core/Interface/IFundOperation.cs
@@ -0,0 +1,20 @@
+﻿using BudgetCalculator.Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator.Core.Interface
+{
+    public interface IFundOperation
+    {
+        decimal Amount { get; set; }
+        DateTime Date { get; set; }
+
+        string Name { get; set; }
+        string Comment { get; set; }
+        Interval Period { get; set; }
+
+    }
+}
---
+++ FamilyBudgetCalculator/Proposal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public interface IBudget
+{
+    decimal Balance { get; set; }
+    List<IExpense> Expenses { get; set; }
+    List<IIncome> Incommings { get; set; }
+
+}
+
+public interface IFundOperation
+{
+    decimal Amount { get; set; }
+    DateTime Date { get; set; }
+
+    string Name { get; set; }
+    string Comment { get; set; }
+    Interval Period { get; set; }
+
+}
+
+public interface IExpenses : IFundOperation
+{
+    ExpenseType TypeOfExpense { get; set; }
+
+
+}
+
+public interface IIncome : IFundOperation
+{
+    //???
+}
+
+public interface IPersonalExpenses : IExpenses
+{
+    IFamilyMember FamilyMember { get; set; }
+}
+
+public interface IFamilyMember
+{
+    string Name { get; }
+
+    sbyte Age { get; }
+
+    FamilyMemberStatus Status { get; }
+}
