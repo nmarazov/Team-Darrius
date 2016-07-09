03a051e493e6abe54ce7772d26ac6a5df8e8fdd2
Stefan <st_dantchev@abv.bg>
2016-07-09 22:06:18 +0300

    Предложение за схема на основните класове

--- FamilyBudgetCalculator/BudgetCalculator.Core/Interface/IBudget.cs
+++ FamilyBudgetCalculator/BudgetCalculator.Core/Interface/IBudget.cs
@@ -1,7 +1,12 @@
-﻿namespace BudgetCalculator.Core.Interface
+﻿using System.Collections.Generic;
+
+namespace BudgetCalculator.Core.Interface
 {
     public interface IBudget
     {
         decimal Balance { get; set; }
+        List<IExpenses> Expenses { get; set; }
+        List<IIncome> Incommings { get; set; }
+
     }
 }
