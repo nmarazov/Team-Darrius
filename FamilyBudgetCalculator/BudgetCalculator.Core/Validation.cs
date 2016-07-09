39195b7d82b9c0c22874f643e22e9396be7bc07a
Stefan <st_dantchev@abv.bg>
2016-07-09 18:51:53 +0300

    proba kak raboti github

---
+++ FamilyBudgetCalculator/BudgetCalculator.Core/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetCalculator.Core
+{
+    public static class Validation
+    {
+        public static void CheckIfStringLengthIsValid(string text, int max, int min = 0, string message = null)
+        {
+            if (text.Length < min || max < text.Length)
+            {
+                throw new IndexOutOfRangeException(message);
+            }
+        }
+
+        public static void CheckIfPositive(decimal number, string message = null)
+        {
+            if (number < 0)
+            {
+                throw new IndexOutOfRangeException(message);
+            }
+        }
+
+    }
+}
