6834899748872c5a753067fce5be9b34acb6d475
Hristo Lilov <hristolilov1@gmail.com>
2016-07-10 18:30:34 +0300

    Starting to implement expenses fromstring()

--- FamilyBudgetCalculator/BudgetCalculator.Core/FamilyBudget/Expenses/Accommodation/AccommodationExpenses.cs
+++ FamilyBudgetCalculator/BudgetCalculator.Core/FamilyBudget/Expenses/Accommodation/AccommodationExpenses.cs
@@ -4,7 +4,7 @@
     using BudgetCalculator.Core.Enum;
     using Interface;
 
-    public abstract class AccommodationExpenses : Expenses, IAccomodationExpenses
+    public class AccommodationExpenses : Expenses, IAccomodationExpenses
     {
         private AccomodationExpenseType accommodationType;
 
@@ -26,6 +26,19 @@
         public override string ToString()
         {
             return base.ToString() + "#" + this.accommodationType.ToString();
+        }
+
+        public static AccommodationExpenses FromString(string input)
+        {
+            string[] split = input.Split('#');
+            decimal value = decimal.Parse(split[1]);
+            string comment = split[2];
+            Interval interval = (Interval)System.Enum.Parse(typeof(Interval), split[3], true);
+            PaymentType paymentType = (PaymentType)System.Enum.Parse(typeof(PaymentType), split[4], true);
+            AccomodationExpenseType accExpenseType = (AccomodationExpenseType)System.Enum.Parse(typeof(AccomodationExpenseType), split[6], true);
+            DateTime date = DateTime.Parse(split[5]);
+            //TypeOfExpense.ToString(), Amount.ToString(), Comment.ToString(), Period.ToString(), WayOfPayment.ToString(), Date.ToString()
+            return new AccommodationExpenses(value,comment,interval,paymentType,accExpenseType,date);
         }
     }
 }
